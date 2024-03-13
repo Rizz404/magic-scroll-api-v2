1d2c54c37e6415d9ac5f6377096415485d94bf1f
Rizqiansyah Ramadhan <thenoblerizz@gmail.com>
2024-03-13 14:21:53 +0700

    add allowed role

---
+++ src/middleware/allowedRoles.ts
@@ -0,0 +1,21 @@
+import { RequestHandler } from "express";
+import { getErrorMessage } from "../utils/express";
+
+const allowedRoles: (allowedRoles: Array<"ADMIN" | "USER">) => RequestHandler =
+  (allowedRoles) => async (req, res, next) => {
+    try {
+      if (!req.user) return res.status(401).json({ message: "Need authentication" });
+
+      const { role } = req.user;
+
+      if (!allowedRoles) return res.status(400).json({ message: "Must fill at least one role" });
+
+      if (!allowedRoles.includes(role)) {
+        return res.status(403).json({ message: `Only ${allowedRoles.join(", ")} allowed` });
+      }
+    } catch (error) {
+      res.status(500).json({ message: getErrorMessage(error) });
+    }
+  };
+
+export default allowedRoles;
